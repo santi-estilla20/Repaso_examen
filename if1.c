3a4c2c6fd64a73872e154c41ba5ff860b12f7713
santi-estilla20 <os967577@gmail.com>
2026-05-21 16:08:39 -0600

    Create if1.c

---
+++ if1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    num = 0;
+    
+    printf("Ingrese un numero entero:\n");
+    scanf("%i", &num);
+    
+    if (num > 100)
+    {
+        printf("El número es mayor a 100");
+    }
+    
+return 0;
+}
+    
