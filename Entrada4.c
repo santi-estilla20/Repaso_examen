61982f13f10d715996b1c89bcfb9aadd79fc94db
santi-estilla20 <os967577@gmail.com>
2026-05-21 16:03:44 -0600

    Create Entrada4.c

---
+++ Entrada4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int temp;
+    
+    printf("Ingrese un valor para la variable a y otro para la variable b, separados por coma:\n");
+    scanf("%i,%i");
+    
+    a = temp;
+    temp = a;
+    b = temp;
+    
+    printf("El valor de a es %i y el valor de b es %i", a, b);
+    
+return 0;
+}
+    
